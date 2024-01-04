8fce200aab897f2888bce0f1e76402ca5513996d
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2024-01-03 23:00:55 -0700

    added to data.js and updated navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+import Footer from "./Components/Footer";
+import React from "react";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Navbar />
       <Hero />
-    </>
+      <Services />
+      <Tours />
+      <Footer />
+    </React.Fragment>
   );
 }
 export default App;
